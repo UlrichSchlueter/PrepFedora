ae51d0e40b5b4030636e87bc5035f817846454fa
Ulrich Schlueter <ulrich.schlueter@gmail.com>
2020-11-07 11:45:09 +0100

    add mixed, probably optional things

---
+++ mixedthings/installOdo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#From 
+#https://odo.dev/docs/installing-odo/#installing-odo-in-visual-studio-code-vscode
+
+#for Linux do:
+
+
+curl -L https://mirror.openshift.com/pub/openshift-v4/clients/odo/latest/odo-linux-amd64 -o /usr/local/bin/odo
+
+chmod +x /usr/local/bin/odo
